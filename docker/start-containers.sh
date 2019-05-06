e9e885f2f1a8c8cc793be21634823d40210fe0ad
Robert Kaye <rob@musicbrainz.org>
2019-05-06 14:14:36 +0200

    Add restart flag

--- docker/start-containers.sh
+++ docker/start-containers.sh
@@ -3,9 +3,9 @@
 docker run -d \
     --expose 3031 \
     --name immutableweb-org \
-    -v /home/website/immutableweb.org:/code/iw \
     --env "VIRTUAL_HOST=immutableweb.org" \
     --env "LETSENCRYPT_HOST=immutableweb.org" \
     --env "LETSENCRYPT_EMAIL=mayhem@gmail.com" \
     --network=website-network \
+    --restart unless-stopped \
     immutableweb-org:beta
